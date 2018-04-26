030c8e9fca113758e393030986fc62d06b956a14
Aimin Yan <aimin.at.work@gmail.com>
2018-04-26 18:33:57 -0400

    add usage

--- config/clusterGeneric_LSF/run.sh
+++ config/clusterGeneric_LSF/run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# usage : sh Aimin_project/BigDataScript/config/clusterGeneric_LSF/run.sh \ 
+#         bbc CufflinksAnno 48:00 8 general 3686 echo "ok"
+
 echo "$7" | bsub  \
        -P $1 \
        -J $2 \
